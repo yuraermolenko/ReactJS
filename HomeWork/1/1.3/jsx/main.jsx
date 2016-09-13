47aa6a716ab1cf72dd7f3db7590d9d1f885c9833
yuraermolenko <yura14ermolenko@gmail.com>
2016-09-13 16:36:31 +0300

    1.3

---
+++ HomeWork/1/1.3/jsx/main.jsx
@@ -0,0 +1,10 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+var MyElement = React.createClass({
+    
+    render: function () {
+        return (<p style={{ "color": this.props.color, "fontSize":this.props.fontSize}}>Hello, world!</p>)
+    }
+});
+var container = document.getElementById('example');
+ReactDOM.render(<MyElement color="red" fontSize="20px" />, container);
