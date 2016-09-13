8d8e0c311b2fd822226183bbc12feac615104058
yuraermolenko <yura14ermolenko@gmail.com>
2016-09-13 21:27:30 +0300

    3

---
+++ ClassWork/3/3.2/jsx/main.jsx
@@ -0,0 +1,29 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+var StopWatch = React.createClass({ 
+
+    getInitialState: function() {
+        return {
+            counter: 1, 
+        } 
+    }, 
+
+    count: function() {
+        this.setState({counter: this.state.counter + 1}); 
+    },
+
+    render: function() {
+                    
+        if (!this.timer) {
+            this.timer = setInterval(this.count, 1000); 
+        }
+
+        return (
+            <div><h1>Прошло секунд с момента открытия страницы : {this.state.counter}</h1></div>
+        )
+        }
+})
+
+var container = document.getElementById('example'); 
+
+ReactDOM.render(<StopWatch/>, container); 
---
+++ ClassWork/3/3.3/jsx/main.jsx
@@ -0,0 +1,32 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+var StopWatch = React.createClass({ 
+
+    getInitialState: function() {
+        return {
+            counter: 1, 
+        } 
+    }, 
+
+    count: function() {
+        this.setState({counter: this.state.counter + 1}); 
+    },
+
+    render: function() {
+                    
+        if (!this.timer) {
+            this.timer = setInterval(this.count, 1000); 
+        }
+
+        return (
+            <div><h1>Секундомер : {this.state.counter}</h1>
+    <button onClick={this.startWatch}>Start</button>
+    <button onClick={this.stopWatch}>Stop</button>
+    <button onClick={this.resetWatch}>Reset</button></div>
+        )
+}
+})
+
+var container = document.getElementById('example'); 
+
+ReactDOM.render(<StopWatch/>, container); 
