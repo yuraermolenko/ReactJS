a2e3b1bc6a0123b2da8bf0410ab7afdd5a0bd5aa
yuraermolenko <yura14ermolenko@gmail.com>
2016-09-14 16:54:03 +0300

    3.3

--- ClassWork/3/3.1/jsx/main.jsx
+++ ClassWork/3/3.1/jsx/main.jsx
@@ -1,1 +1,20 @@
-﻿
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+var Element = React.createClass({
+    getInitialState:function(){
+        return { classToggle: true }
+    },
+    changeClass:function(){
+        this.setState({classToggle:!this.state.classToggle})
+    },
+    render: function () {
+        return(
+           <div>
+               <button onClick={this.changeClass}>Change Class!</button>
+               <div className={this.state.classToggle? "black" : "red" }></div>     
+                 </div>
+    )
+    }
+})
+var container = document.getElementById('example');
+ReactDOM.render(<Element></Element>, container);
