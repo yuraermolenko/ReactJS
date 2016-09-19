ee262fd83a1dfa227b50d6ffa7b39b65130de278
yuraermolenko <yura14ermolenko@gmail.com>
2016-09-20 02:27:00 +0300

    22

--- HomeWork/4/4.2/build/bundle.js
+++ HomeWork/4/4.2/build/bundle.js
@@ -49,8 +49,6 @@
 
 	"use strict";
 	
-	var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -92,39 +90,29 @@
 	var User = function (_Person) {
 	    _inherits(User, _Person);
 	
-	    function User() {
+	    function User(firstName, lastName, age, gender) {
+	        var signUpToDate = arguments.length <= 4 || arguments[4] === undefined ? "01.01.1970" : arguments[4];
+	        var id = arguments[5];
+	
 	        _classCallCheck(this, User);
 	
-	        var _this = _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).call(this, firstName = "John", lastName = "Doe", age = 0, gender = "Male", signUpToDate = "01.01.1970", id = 1));
+	        var _this = _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).call(this, firstName, lastName, age, gender));
 	
 	        _this.signUpToDate = signUpToDate;
 	        _this.id = id;
 	        return _this;
 	    }
 	
-	    _createClass(User, [{
-	        key: "fullName",
-	        value: function fullName() {
-	            _get(User.prototype.__proto__ || Object.getPrototypeOf(User.prototype), "fullName", this).call(this);
-	        }
-	    }, {
-	        key: "sayHi",
-	        value: function sayHi() {
-	            _get(User.prototype.__proto__ || Object.getPrototypeOf(User.prototype), "sayHi", this).call(this);
-	        }
-	    }, {
-	        key: "signUpToDate",
-	        value: function signUpToDate(date) {}
-	    }]);
-	
 	    return User;
 	}(Person);
 	
+	;
+	
 	var user1 = new User("Alastor", "Green", 35, "Male", "10.09.2016", 123);
-	var user2 = new User("Name1", "Surnameme", 35, "Male", "11.09.2016", 1234);
-	var user3 = new User("Name2", "Green", 35, "Female", "12.09.2016", 12345);
-	var user4 = new User("Name3", "Green", 35, "Male", "13.09.2016", 123456);
-	var user5 = new User("Name4", "Green", 35, "Female", "14.09.2016", 1234567);
+	var user2 = new User("Name1", "Surname", 32, "Male", "11.09.2016", 1234);
+	var user3 = new User("Name2", "Green", 31, "Female", "12.09.2016", 12345);
+	var user4 = new User("Name3", "Green", 30, "Male", "13.09.2016", 123456);
+	var user5 = new User("Name4", "Green", 13, "Female", "14.09.2016", 1234567);
 	var users = [user1, user2, user3, user4, user5];
 
 /***/ }
