f4a8f1d42377f97ca02275eed44cb0f4de72953b
yuraermolenko <yura14ermolenko@gmail.com>
2016-09-19 12:19:30 +0300

    hghg

--- HomeWork/4/4.2/jsx/main.jsx
+++ HomeWork/4/4.2/jsx/main.jsx
@@ -13,27 +13,20 @@
         }
     }
     class User extends Person{
-        constructor(){
-        super(firstName="John",lastName="Doe",age=0,gender="Male",signUpToDate="01.01.1970",id=1);
-        this.signUpToDate=signUpToDate;
-        this.id=id;
-    }
-        fullName(){
-        super.fullName();
-    }
-    sayHi(){
-        super.sayHi();
-    }
-    signUpToDate(date){
-
-    }
-    }
+        constructor(firstName,lastName,age,gender,signUpToDate="01.01.1970",id){
+            super(firstName,lastName,age,gender);
+            this.signUpToDate=signUpToDate;
+            this.id=id;
+        }
+   
+   
+    };
 
 let user1=new User("Alastor","Green",35,"Male","10.09.2016",123);
-let user2=new User("Name1","Surnameme",35,"Male","11.09.2016",1234);
-let user3=new User("Name2","Green",35,"Female","12.09.2016",12345);
-let user4=new User("Name3","Green",35,"Male","13.09.2016",123456);
-let user5=new User("Name4","Green",35,"Female","14.09.2016",1234567);
+let user2=new User("Name1","Surname",32,"Male","11.09.2016",1234);
+let user3=new User("Name2","Green",31,"Female","12.09.2016",12345);
+let user4=new User("Name3","Green",30,"Male","13.09.2016",123456);
+let user5=new User("Name4","Green",13,"Female","14.09.2016",1234567);
     let users=[user1,user2,user3,user4,user5];
         
         
